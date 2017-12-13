c3f9cfdfd06032b758825194d9bb0e417bd002ce
tomliang880611 <liangkai1234@gmail.com>
2017-12-14 02:22:05 +0800

    draft version

--- test_pandas.py
+++ test_pandas.py
@@ -36,8 +36,72 @@ class ColumnPreProcessor():
 
 class CompareConfig():
     column_suffixes = ('_src', '_target')
-    def __init__(self):
-        pass
+    compare_value_column = 'value'
+    src_processors = []
+    target_processors = []
+
+    def __init__(self,
+                 compare_column='value'):
+        self.compare_value_column = compare_column
+        # if(not (src and target)):
+        #     raise ValueError('src and target file path should be specified.')
+
+    def append_processor(self,
+                         from_column,
+                         to_column=None,
+                         transforms=[],
+                         filters=[],
+                         src_flag=True):
+        if to_column is None:
+            to_column = from_column
+
+        # build processor
+        processor = ColumnPreProcessor(from_column, to_column)
+        if(len(transforms) > 0):
+            for t in transforms:
+                processor.add_transformer(t)
+
+        if(len(filters) > 0):
+            for f in filters:
+                processor.add_filter(f)
+
+        # assignment
+        if(src_flag):
+            self.src_processors.append(processor)
+        else:
+            self.target_processors.append(processor)
+
+    def get_join_columns(self):
+        def _extract_column_name(processor):
+            return processor.to_col_name
+
+        src_columns = set(map(_extract_column_name, self.src_processors))
+        target_columns = set(map(_extract_column_name, self.target_processors))
+
+        if(src_columns == set() or target_columns == set()):
+            return list(src_columns.union(target_columns))
+
+        if(src_columns == target_columns):
+            return list(src_columns)
+
+        raise ValueError("src column {} and target column {} are different"
+                         .format(src_columns, target_columns))
+
+    def get_compare_columns(self):
+        select_keys_list = []
+        for k in self.column_suffixes:
+            select_keys_list.append(self.compare_value_column + k)
+        return select_keys_list
+
+    def get_result_value_column(self):
+        return self.compare_value_column + "_diff"
+
+    def get_result_columns(self):
+        result_columns = []
+        result_columns.extend(config.get_join_columns())
+        result_columns.extend(config.get_compare_columns())
+        result_columns.extend([config.get_result_value_column()])
+        return result_columns
 
 
 def handle_column_proprocessor(df, processor):
@@ -50,26 +114,53 @@ def handle_column_proprocessor(df, processor):
         df[processor.to_col_name] = tmp
 
 
-def get_select_keys():
-    return ['A', 'B', 'value_src', 'value_target']
+# renovation
+src_df2 = pd.read_csv('./testdata.csv')
+target_df2 = pd.read_csv('./testdata_target.csv')
+
+config = CompareConfig('value')
+config.append_processor('A1',
+                        to_column='A',
+                        transforms=["x.strip('prefix_')"],
+                        src_flag=False)
+config.append_processor('B2', to_column='B', src_flag=False)
+
+for processor in config.target_processors:
+    handle_column_proprocessor(target_df2, processor)
+
+merged_df2 = pd.merge(src_df2, target_df2,
+                      on=config.get_join_columns(),
+                      suffixes=config.column_suffixes)
+merged_df2[config.get_result_value_column()]\
+    = merged_df2[config.get_compare_columns()[0]] == \
+    merged_df2[config.get_compare_columns()[1]]
+
+merged_df2[config.get_result_columns()]
 
 
-target_preprocessor = ColumnPreProcessor("A1", "A")
-target_preprocessor.add_filter("not x.startswith('prefix_')")
-target_preprocessor.add_transformer("x.strip('prefix_')")
+# def draft_action():
 
-target_preprocessor2 = ColumnPreProcessor("B2", "B")
+#     def get_select_keys():
+#         return ['A', 'B', 'value_src', 'value_target', 'value_diff']
 
-# data washing
-src_df = pd.read_csv('./testdata.csv')
-target_df = pd.read_csv('./testdata_target.csv')
+#     target_preprocessor = ColumnPreProcessor("A1", "A")
+#     target_preprocessor.add_filter("not x.startswith('prefix_')")
+#     target_preprocessor.add_transformer("x.strip('prefix_')")
 
-handle_column_proprocessor(target_df, target_preprocessor)
-handle_column_proprocessor(target_df, target_preprocessor2)
+#     target_preprocessor2 = ColumnPreProcessor("B2", "B")
 
-merged_df = pd.merge(src_df,
-                     target_df, on=['A', 'B'],
-                     suffixes=('_src', '_target'))
+#     src_df = pd.read_csv('./testdata.csv')
+#     target_df = pd.read_csv('./testdata_target.csv')
 
-select_keys = get_select_keys()
-merged_df[select_keys]
+#     handle_column_proprocessor(target_df, target_preprocessor)
+#     handle_column_proprocessor(target_df, target_preprocessor2)
+
+#     merged_df = pd.merge(src_df,
+#                          target_df, on=['A', 'B'],
+#                          suffixes=('_src', '_target'))
+
+#     merged_df['value_diff']\
+#         = merged_df['value_src'] == merged_df['value_target']
+
+#     select_keys = get_select_keys()
+#     merged_df[select_keys]
